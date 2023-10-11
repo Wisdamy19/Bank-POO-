d00dc066c4a83eae724d2bb52a3403b8724c300e
Antônio W. Saraiva <126352226+Wisdamy19@users.noreply.github.com>
2023-10-11 02:28:36 -0300

    Add files via upload

---
+++ AccountBankMembers.java
@@ -0,0 +1,66 @@
+package Secao09.AccountBankPOO;
+
+public class AccountBankMembers {
+    private int account;
+    public String name;
+    public double depositInitial;
+    public double deposit;
+    public String answer;
+    public double withdraw;
+    public double balance = 00.00;
+
+    public AccountBankMembers() {
+        this.account = account;
+        this.name = name;
+        this.depositInitial = depositInitial;
+        this.answer = answer;
+        this.withdraw = withdraw;
+        this.balance = balance;
+    }
+
+    public int setAccount(int account) {
+        this.account = account;
+        return account;
+    }
+    public int getAccount(){
+        return account;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public double addDeposit(){
+        return depositInitial += deposit;
+    }
+    public double withdrawValue(){
+        return  depositInitial - withdraw;
+    }
+    public double taxValue(){
+        return withdrawValue() - 5.00f;
+    }
+    public double negativeValue(){
+        return (deposit - withdraw - 5.00f);
+    }
+    public String toString(){
+        return "Account Data: " + this.account + ", " + "Holder: " + this.getName() + ", " + "Balance: " + "$" + this.depositInitial;
+    }
+    public String toString2(){
+        return "Account Data: " + this.account + ", " + "Holder: " + this.getName() + ", " + "Balance: " + "$" + addDeposit();
+    }
+    public String toString3(){
+        return "Account Data: " + this.account + ", " + "Holder: " + this.getName() + ", " + "Balance: " + "$" + taxValue();
+    }
+    public String toString4(){
+        return "Updated account data: %n" + this.account + ", " + "Holder: " + this.name + ", " + "Balance: " + addDeposit();
+    }
+    public String toString5(){
+        return "Updated account data: " + this.account + ", " + "Holder: " + this.name + ", " + "Balance: " + getBalance();
+    }
+    public String toString6(){
+        return "Updated account data: " + this.account + ", " + "Holder: " + this.name + ", " + "Balance: " + negativeValue();
+    }
+
+
+}
